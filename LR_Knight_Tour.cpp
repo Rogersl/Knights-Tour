6b835e8a63d5989265ce292fbde6a6f987c7dd4a
Logan Rogers <ljr9z9@mst.edu>
2017-09-25 22:21:55 -0500

    Updated code and name.

--- LR_Knight_Tour.cpp
+++ LR_Knight_Tour.cpp
@@ -1,27 +1,5 @@
 //Logan Rogers
 
-/*
-This program takes an NxN board to find a Closed Knight's Tour.
-
-The value of boardSize can be modified to easily try this program out for
-any given boardSize.  However, bear in mind that a closed tour may only be found
-for any N >= 6 that is even.  Odd values and lesser values cannot complete a closed
-tour.
-
-This program utilizes recursive backtracking and Warnsdorff's Rule to find a
-closed tour.  Warnsdorff's Rule takes an initial starting position and
-searches for subsequent locations with the fewest moves, moving to them.  
-
-We use this approach to get near a closed tour.  However, achieving a closed
-tour is not guaranteed with Warndorff's Rule.  This is why we also employ 
-recursive backtracking.  We move back steps taken in Warndorff's Rule until
-we arrive at a position to finish a closed tour.
-
-While this may increase complexity a bit, the backtracking usually only involves
-the final steps of Warnsdorff's Rule, meaning that we don't have to backtrack far
-to reach our closed tour.  
-*/
-
 #include <iostream>
 
 using namespace std;
